b883107a4a1507fc26004dda64293db3537ce42f
Raphael Rodrigues da Costa <rxonda@gmail.com>
2015-07-02 10:09:11 -0300

    Mudando a descricao do plugin

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :csv_teste do
   name 'Csv Teste plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Raphael R. Costa'
+  description 'Plugin para gerar arquivo CSV de lancamentos de timesheet'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
