d8acfb1b455972721f0b0e969ac2e50c91208c41
Raphael Rodrigues da Costa <rxonda@gmail.com>
2015-07-02 10:36:36 -0300

    Teste de pagina

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+get 'csv-exporter', :to => 'exporter#export'
--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :csv_teste do
   author 'Raphael R. Costa'
   description 'Plugin para gerar arquivo CSV de lancamentos de timesheet'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/rxonda/cvs_redmine_plugin'
+  author_url 'https://github.com/rxonda'
 end
