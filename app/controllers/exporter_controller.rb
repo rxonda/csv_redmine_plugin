d2527c530c4cb7f38a2450d8c300fda51b20c1d9
Raphael Rodrigues da Costa <rxonda@gmail.com>
2015-07-03 10:48:10 -0300

    mais validacoes 6

--- app/controllers/exporter_controller.rb
+++ app/controllers/exporter_controller.rb
@@ -5,8 +5,8 @@ class ExporterController < ApplicationController
   def export
 	@inicio = params[:dataInicio]
 	@fim = params[:dataTermino]
-  	if params[:dataInicio] || !params[:dataInicio].strip.empty? then
-  		if !params[:dataTermino] || params[:dataTermino].strip.empty?
+  	if !params[:dataInicio].blank? then
+  		if params[:dataTermino].blank?
   			flash[:error] = 'Você deve informar a Data de término'
   		elsif @inicio > @fim
   			flash[:error] = 'Data de início não pode maior que a Data de término!'
@@ -14,7 +14,7 @@ class ExporterController < ApplicationController
 	  		@timeEntries = TimeEntry.where("spent_on >= :start_date and spent_on <= :end_date", 
 	  			{start_date: params[:dataInicio], end_date: params[:dataTermino]})
   		end
-  	elsif !params[:dataTermino] || params[:dataTermino].strip.empty?
+  	elsif params[:dataTermino].blank?
   		flash[:error] = 'Você deve informar a Data de início'
   	else
     	@timeEntries = TimeEntry.all
