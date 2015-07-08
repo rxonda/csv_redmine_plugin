0d467572dd6f7d20ddb6d65fc1e66e0555f07d03
Raphael Rodrigues da Costa <rxonda@gmail.com>
2015-07-08 15:13:29 -0300

    alterando o nome do plugin

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 Holidays::TIPOS_FERIADOS = [:br,:observed,:informal]
 
-Redmine::Plugin.register :cvs_redmine_plugin do
+Redmine::Plugin.register :csv_redmine_plugin do
   name 'Csv Teste plugin'
   author 'Raphael R. Costa'
   description 'Plugin para gerar arquivo CSV de lancamentos de timesheet'
   version '0.0.1'
-  url 'https://github.com/rxonda/cvs_redmine_plugin'
+  url 'https://github.com/rxonda/csv_redmine_plugin'
   author_url 'https://github.com/rxonda'
 
   menu :application_menu, :exporter, { :controller => 'exporter', :action => 'export' }, :caption => 'Exportação'
