b3cf696c9ad5d673a651983495f5f38c9a8e64cb
Raphael Rodrigues da Costa <rxonda@gmail.com>
2015-07-14 14:54:13 -0300

    Corrigindo codigo para suportar ruby 1.8 codigo da view 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'exporter/timesheet' => 'exporter#index', :as => 'exporter_timesheet'
+get 'exporter/export' => 'exporter#export', :as => 'exporter_export'
